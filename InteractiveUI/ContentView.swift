572cdf67e2b9934d2f6598418904ec17acc3e580
r-shankaran <riyashankaran18@gmail.com>
2025-07-23 13:48:02 -0700

    Added functionality

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -8,16 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    // @State is a property wrapper that monitors changes and tells SwiftUI to update the view when the var is modified
+    @State private var name = ""
+    @State private var textTitle = "What is your name?"
     var body: some View {
         VStack {
-            Text("What is your name?")
+            Text(textTitle)
                 .font(.title)
-            TextField("Type your name here...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+            TextField("Type your name here...", text: $name)
                 .multilineTextAlignment(.center)
                 .font(.title)
                 .border(Color.gray, width: 1)
             Button("Submit Name") {
-                
+                textTitle = "Welcome, \(name)!"
             }
             .font(.title2)
             .buttonStyle(.borderedProminent)
